484528ada002aa325c7db55bd5fbc533484c1686
Diego Morais <diegormorais@live.com>
2021-03-28 19:39:05 -0300

    varios i18n implementados

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,8 +1,8 @@
 class Promotion < ApplicationRecord
     has_many :coupons, dependent: :destroy
     
-    validates :name, :code, :discount_rate, :coupon_quantity, :expiration_date, presence: { message: 'não pode ficar em branco'}
-    validates :name, :code, uniqueness: { message: 'deve ser único' }
+    validates :name, :code, :discount_rate, :coupon_quantity, :expiration_date, presence: true
+    validates :name, :code, uniqueness: true
 
     def generate_coupons!
         return unless coupons.empty?
@@ -13,6 +13,7 @@ class Promotion < ApplicationRecord
         end
     end
 
+    # TODO: fazer testes para esse método
     def coupons?
         coupons.any?
     end
--- test/system/promotions_test.rb
+++ test/system/promotions_test.rb
@@ -84,7 +84,7 @@ class PromotionsTest < ApplicationSystemTestCase
     fill_in 'Desconto', with: '15'
     fill_in 'Quantidade de cupons', with: '90'
     fill_in 'Data de término', with: '22/12/2033'
-    click_on 'Criar promoção'
+    click_on 'Criar Promoção'
 
     assert_current_path promotion_path(Promotion.last)
     assert_text 'Cyber Monday'
@@ -106,7 +106,7 @@ class PromotionsTest < ApplicationSystemTestCase
     # fill_in 'Desconto', with: ''
     # fill_in 'Quantidade de cupons', with: ''
     # fill_in 'Data de término', with: ''
-    click_on 'Criar promoção'
+    click_on 'Criar Promoção'
 
     assert_text 'não pode ficar em branco', count: 5
   end
@@ -121,9 +121,9 @@ class PromotionsTest < ApplicationSystemTestCase
     click_on 'Registrar uma promoção'
     fill_in 'Código', with: 'NATAL10'
     fill_in 'Nome', with: 'Natal'
-    click_on 'Criar promoção'
+    click_on 'Criar Promoção'
 
-    assert_text 'deve ser único', count: 2
+    assert_text 'já está em uso', count: 2
   end
 
   test 'generate coupons for a promotion' do
