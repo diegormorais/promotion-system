d02eaf07e4d04935a10bbfa6518652cde6e0b0ef
Diego Morais <diegormorais@live.com>
2021-04-16 19:22:16 -0300

    testes de API

--- test/integration/promotion_api_test.rb
+++ test/integration/promotion_api_test.rb
@@ -4,14 +4,40 @@ class PromotionApiTest < ActionDispatch::IntegrationTest
     test 'show coupon' do
         user = User.create!(email: 'john.doe@iugu.com.br', password: '123456')
         promotion = Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
-                                      code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
+                                      code: 'NATAL10', discount_rate: 15, coupon_quantity: 100,
                                       expiration_date: '22/12/2033', user: user)
         
         coupon = Coupon.create!(code: 'NATAL10-0001', promotion: promotion)
 
-        get "/api/v1/coupons/#{coupon.code}"
+        get "/api/v1/coupons/#{coupon.code}", as: :json
 
         assert_response :success
-        assert_equal coupon.code, responde.parsed_body['code']
+        assert_equal promotion.discount_rate.to_s, response.parsed_body['discount_rate']
+    end
+
+    test 'show coupon not found' do
+        get '/api/v1/coupons/0', as: :json
+
+        assert_response :not_found
+    end
+
+    test 'route invalid without json header' do
+        assert_raises ActionController::RoutingError do
+            get '/api/v1/coupons/0'
+        end
+    end
+
+    test 'show coupon disabled' do
+        user = User.create!(email: 'john.doe@iugu.com.br', password: '123456')
+        promotion = Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
+                                      code: 'NATAL10', discount_rate: 15, coupon_quantity: 100,
+                                      expiration_date: '22/12/2033', user: user)
+        
+        coupon = Coupon.create!(code: 'NATAL10-0001', promotion: promotion, status: :disabled)
+
+        get "/api/v1/coupons/#{coupon.code}", as: :json
+
+        assert_response :success
+        assert_equal promotion.discount_rate.to_s, response.parsed_body['discount_rate']
     end
 end
