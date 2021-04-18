c549c5e79a9da4b76be1f89ad662d6109f372e82
Diego Morais <diegormorais@live.com>
2021-04-18 14:42:38 -0300

    Implementada a Gem Fabricator

---
+++ test/fabricators/promotion_fabricator.rb
@@ -0,0 +1,9 @@
+Fabricator(:promotiom) do
+    name 'Natal'
+    description 'Promoção de natal'
+    code { sequence(:code) { |i| "NATAL#{i}" } }
+    discount_rate 15
+    coupon_quantity 3
+    expiration_date '22/12/2033'
+    user
+end
